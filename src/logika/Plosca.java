c13a4e0059f18ee02cdc72633a23d5e5ba54f428
Metod Jazbec <jazbec.metod@gmail.com>
2018-04-26 12:20:19 +0200

    inicalizacija 2

--- src/logika/Plosca.java
+++ src/logika/Plosca.java
@@ -24,8 +24,13 @@ public class Plosca {
 	public void dodajPovezavo(int i, int j){
 		if (i != j){
 			vozlisce(i).sosedi.add(j);
-			vozlisce(j).sosedi.add(i);
+			//vozlisce(j).sosedi.add(i);
 		}
+	}
+	
+	//Pretvori double v int
+	public static int round(double x) {
+		return (int)(x + 0.5);
 	}
 	
 	public void incializacija() {
@@ -35,47 +40,66 @@ public class Plosca {
 		for (int i = 1; i <= 11; i++) {
 			for (int j = 1; j <= i; j++ ) {
 				Vozlisce a = new Vozlisce(i, j); //pozor: tole niso obiène karteziène koordinate
+				this.tabelaVozlisc[indeks] = a;
 				if (i <= 11 && j == 1) {
 					a.rob.add(Rob.r1);
-				} else if (i >= 11 && j == 1) {
+					dodajPovezavo(indeks, indeks +1);
+					dodajPovezavo(indeks, indeks + i);
+					dodajPovezavo(indeks, indeks -i + 1);
+					dodajPovezavo(indeks, (indeks +1 + i) * (1 - round(Math.pow(0, 11 -i ))));
+				} if (i >= 11 && j == 1) {
 					a.rob.add(Rob.m2);
-				} else if (i == j) {
+				}  if (i == j) {
 					a.rob.add(Rob.m1);
-				} else if (i + j == 22) {
+					dodajPovezavo(indeks, (indeks +i) * (1 - round(Math.pow(0, 11 -i ))));
+					dodajPovezavo(indeks, Math.max(indeks - 1,0));
+					dodajPovezavo(indeks, (indeks +1+i) * (1 - round(Math.pow(0, 11 -i ))));
+					dodajPovezavo(indeks, Math.max(indeks - i,0));
+				} if (i + j == 22) {
 					a.rob.add(Rob.r2);
-				} else {		// imamo notranje vozlišèe, dodamo 6 sosedov
-					a.sosedi.add(indeks+1);
-					a.sosedi.add(indeks-1);
-					a.sosedi.add(indeks+i);
-					a.sosedi.add(indeks-i);
-					a.sosedi.add(indeks+i+1);
-					a.sosedi.add(indeks-i+1);
+				} if(j > 1 && j < i) {		// imamo notranje vozlišèe, dodamo 6 sosedov
+					dodajPovezavo(indeks, indeks+1);
+					dodajPovezavo(indeks, indeks-1);
+					dodajPovezavo(indeks, indeks+i);
+					dodajPovezavo(indeks, indeks-i);
+					dodajPovezavo(indeks, indeks+i+1);
+					dodajPovezavo(indeks, indeks-i+1);
 				}
-				this.tabelaVozlisc[indeks] = a;
 				indeks ++;
 			}
 		}
 		
 		//zgornji trikotnik romba
 		for (int i = 12; i <= 21; i++) {
+			int l = 22 - i;
 			for (int j = 1; j <= 22 - i; j++ ) {
 				Vozlisce a = new Vozlisce(i, j); //pozor: tole niso obiène karteziène koordinate
+				this.tabelaVozlisc[indeks] = a; 
 				if (i >= 11 && j == 1) {
 					a.rob.add(Rob.m2);
-				} else if (i + j == 22) {
+					dodajPovezavo(indeks, Math.min(indeks+1,120));
+					dodajPovezavo(indeks, Math.min(indeks+l,120));
+					dodajPovezavo(indeks, indeks - l - 1);
+					dodajPovezavo(indeks, indeks - l);
+
+				} if (i + j == 22) {
 					a.rob.add(Rob.r2);
-				} else {		// imamo notranje vozlišèe, dodamo 6 sosedov
-					a.sosedi.add(indeks+1);
-					a.sosedi.add(indeks-1);
-					a.sosedi.add(indeks+22-i);
-					a.sosedi.add(indeks-(22-i));
-					a.sosedi.add(indeks+22-i+1);
-					a.sosedi.add(indeks-(22-i)+1);
+					dodajPovezavo(indeks, indeks - 1);
+					dodajPovezavo(indeks, indeks + l -1);
+					dodajPovezavo(indeks, indeks -l -1);
+					dodajPovezavo(indeks, indeks - l);
+				} if(j > 1 && j < l) {		// imamo notranje vozlišèe, dodamo 6 sosedov
+					dodajPovezavo(indeks, indeks+1);
+					dodajPovezavo(indeks, indeks-1);
+					dodajPovezavo(indeks, indeks+l);
+					dodajPovezavo(indeks, indeks-(l));
+					dodajPovezavo(indeks, indeks+l+1);
+					dodajPovezavo(indeks, indeks-(l)+1);
 				}
-				this.tabelaVozlisc[indeks] = a;
 				indeks ++;
 			}
 		}
+		
 	
 		//TODO: dodaj sosede robnim vozlišèem (tistim, ki imajo 2, 3 ali 4 sosede)
 		
